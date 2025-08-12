149979f263b57fd4c49ce177dca29168a3f8a78e
Jimmy Tran <quantrannhu@gmail.com>
2025-08-12 13:58:59 +0000

    finish react-modal

---
+++ react-modal/my-app/src/App.tsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import './App.css'
+import { Modal } from './Modal'
+
+function App() {
+  const [isOpen,setIsOpen]=useState(false);
+  // not know how to use Tailwind in custom Reactnode yet.
+  return (
+    <>
+    <button onClick={()=>setIsOpen(true)}> Delete Me!</button>
+    <Modal isOpen={isOpen} >
+      <p style={{fontSize:'24px'}}>Do you really want to delete?</p>
+      <div style={{display:'flex', justifyContent:'space-between', margin:'12px', padding:'12px'}}>
+        <div><button onClick={()=>setIsOpen(false)}>Cancel</button></div>
+        <div><button onClick={()=> {alert('Deleting...'); setIsOpen(false)}}>Delete</button></div>
+      </div>
+    </Modal>
+    </>
+  )
+}
+
+export default App
---
+++ react-modal/my-app/src/Modal.tsx
@@ -0,0 +1,19 @@
+import { useEffect, useRef, type ReactNode } from "react"
+
+type Props={
+  children: ReactNode,
+  isOpen:boolean
+}
+
+export function Modal({children, isOpen}:Props){
+  const modal=useRef<HTMLDialogElement>(null)
+  useEffect(()=>{
+        if(isOpen){modal.current?.showModal()}
+        else{modal.current?.close()};
+      },[isOpen])
+  return(
+    <>
+      <dialog ref={modal}>{children}</dialog>
+    </>
+  )
+}
