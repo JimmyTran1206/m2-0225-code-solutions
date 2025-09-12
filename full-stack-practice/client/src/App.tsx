2bed5c4ff549fc658ce17f5de720ba907f21bdaa
Jimmy Tran <quantrannhu@gmail.com>
2025-09-12 05:28:41 +0000

    finish full-stack-practice

--- full-stack-practice/client/src/App.tsx
+++ full-stack-practice/client/src/App.tsx
@@ -1,5 +1,24 @@
-import { Todos } from './components/Todos';
+import { Route, Routes } from 'react-router';
+import './App.css';
+import { Header } from './components/Header';
+import { Catalog } from './pages/Catalog';
+import { ProductDetails } from './pages/ProductDetails';
+import { About } from './pages/About';
+import { NotFound } from './pages/NotFound';
 
-export function App() {
-  return <Todos />;
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Header />}>
+          <Route index element={<Catalog />} />
+          <Route path="products/:productId" element={<ProductDetails />} />
+          <Route path="about" element={<About />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </>
+  );
 }
+
+export default App;
